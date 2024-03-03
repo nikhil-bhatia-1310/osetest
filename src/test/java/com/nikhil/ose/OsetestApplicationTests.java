43ededa4ae633de5f54c65f1e5d767832b758ec4
Nikhil Bhatia <nikhil.bhatia.1310@gmail.com>
2024-03-03 13:34:20 +0530

    code committed

---
+++ src/test/java/com/nikhil/ose/OsetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.ose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
